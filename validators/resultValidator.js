fc5c9257d81d85a1803901943c7071a5198c493d
Arizalb <139053552+Arizalb@users.noreply.github.com>
2024-10-16 07:29:57 +0700

    update submit jawaban tidak boleh kosong

--- validators/resultValidator.js
+++ validators/resultValidator.js
@@ -7,14 +7,24 @@ const submitResultSchema = Joi.object({
       Joi.object({
         questionId: Joi.string().required(),
         type: Joi.string().valid("multiple_choice", "essay").required(),
-        selectedAnswer: Joi.string().when("type", {
-          is: "multiple_choice",
-          then: Joi.string().required(),
-        }),
-        essayAnswer: Joi.string().when("type", {
-          is: "essay",
-          then: Joi.string().required(),
-        }),
+        selectedAnswer: Joi.string()
+          .allow(null, "")
+          .when("type", {
+            is: "multiple_choice",
+            then: Joi.string().required().messages({
+              "string.empty": "Jawaban pilihan ganda tidak boleh kosong.",
+              "any.required": "Jawaban pilihan ganda wajib diisi.",
+            }),
+          }),
+        essayAnswer: Joi.string()
+          .allow(null, "")
+          .when("type", {
+            is: "essay",
+            then: Joi.string().required().messages({
+              "string.empty": "Jawaban esai tidak boleh kosong.",
+              "any.required": "Jawaban esai wajib diisi.",
+            }),
+          }),
       })
     )
     .required(),
@@ -22,9 +32,13 @@ const submitResultSchema = Joi.object({
 
 // Middleware untuk memvalidasi hasil ujian
 const validateSubmitResult = (req, res, next) => {
-  const { error } = submitResultSchema.validate(req.body);
+  const { error } = submitResultSchema.validate(req.body, {
+    abortEarly: false,
+  });
   if (error) {
-    return res.status(400).json({ message: error.details[0].message });
+    return res
+      .status(400)
+      .json({ message: error.details.map((err) => err.message).join(", ") });
   }
   next();
 };
